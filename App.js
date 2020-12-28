ae389f2515fd37f1569f6c37c40be97652448329
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-29 01:36:42 +0600

    Styling Native Component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color:"red"}}>hello world</Text>  
+      <Text>hello world</Text>
+      <Text>hello world</Text>
+      <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +16,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection:"column"
   },
 });
