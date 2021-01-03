8aff95446f3aa818ad7178ae5a25b19814fc8f81
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-03 07:28:20 +0600

    Adding Icons in Tab

--- src/component/navigationTab/navigationTab.jsx
+++ src/component/navigationTab/navigationTab.jsx
@@ -3,16 +3,33 @@ import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SharePlaces from "../SharePlaces/SharePlaces";
 import FindPlaces from "../FindPlaces/FindPlaces";
+import Icons from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const navigationTab = (props) => {
   return (
-      <Tab.Navigator>
-        <Tab.Screen name="Share Places" component={SharePlaces} />
-        <Tab.Screen name="Find Places" component={FindPlaces} />
-      </Tab.Navigator>
-      
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Find Places"
+        component={FindPlaces}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icons name="md-map" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Share Places"
+        component={SharePlaces}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icons name="arrow-redo-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
