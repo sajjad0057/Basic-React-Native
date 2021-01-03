bb589649873d9fe7edcb8ec378fea0084b96a3ec
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-03 06:51:26 +0600

    Setting Up Navigation

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import "react-native-gesture-handler";
 import React from "react";
 import MainComponent from "./src/MainComponent";
 import { Provider } from "react-redux";
 import Store from "./src/redux/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/component/Login/Login";
+
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={Store}>
-      <MainComponent />
-    </Provider>
-  )
+    <NavigationContainer>
+      <Provider store={Store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
 }
